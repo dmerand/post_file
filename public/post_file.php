f6db62320a1a62c37280b452fe6dee6e2b487081
Donald L. Merand <dmerand@gmail.com>
2012-05-24 21:02:31 -0400

    added code to handle DELETE requests as well

--- public/post_file.php
+++ public/post_file.php
@@ -3,18 +3,30 @@
 
 # Set some constants that we'll need later
 $server_name = $_SERVER['SERVER_NAME'];
+$doc_root = $_SERVER["DOCUMENT_ROOT"];
 $server_protocol = "http";
+$request_method = $_SERVER['REQUEST_METHOD'];
 
 # Set an errors variable that we test against to see if we should move forward
 $errors = array();
 
-# This page only responds to POST requests. Add an error if we're doing anything else
-if ($_SERVER['REQUEST_METHOD'] != 'POST') {
-  $errors[] = 'Invalid HTTP request type.';
-}
 
 # This is our awesome security system. Checking passed authentication tokens against a "database" of existing ones.
-$token = $_POST['token'];
+switch($request_method)
+{
+  case 'POST':
+    $token = $_POST['token']; 
+    break;
+	case 'DELETE':
+		parse_str(file_get_contents('php://input'), $_DELETE);
+    $token = $_DELETE['token']; 
+    $dir = $_DELETE['dir'];
+    break;
+  default:
+    # This page only responds to POST + DELETE requests. Add an error if we're doing anything else
+    $errors[] = "Invalid HTTP request type: $request_method.";
+}
+
 $auth_tokens = array(
   "ba38a82e75d4faf026822455026df469" => "Donald"
 );
@@ -27,7 +39,7 @@ if (!array_key_exists($token, $auth_tokens)) {
 # _Any form validation should happen here, and set the $errors variable_
 
 # Page submitted, no errors - set local variables based on POST values
-if (empty($errors)) { 
+if ($request_method == "POST" && empty($errors)) { 
 	# reads the name of the file the user submitted for uploading
 	$file_name=$_FILES['file']['name'];
 	# if it is not empty
@@ -40,7 +52,6 @@ if (empty($errors)) {
     $tmp_name = $_FILES['file']['tmp_name'];
 
     # We're going to store the files in a unique directory, in this case named after a UNIX epoch timestamp.
-    $doc_root = $_SERVER["DOCUMENT_ROOT"];
     $result_dir = time();
     $result_path="$doc_root/$result_dir"; 
     $result_file_path="$result_path/$file_name";
@@ -56,10 +67,32 @@ if (empty($errors)) {
     $move_cmd = "/bin/mv '$tmp_name' '$result_file_path'";
     system($move_cmd, $is_moved);
     if ($is_moved) { $errors[] = 'File move failed'; }
+
+    $result = "$server_protocol://$server_name/$result_dir/$file_name posted.";
   } else {
     $errors[] = "No valid file name.";
   }
 }
+
+
+# DELETE posted, "dir" variable should be a directory
+if ($request_method == 'DELETE' && empty($dir)) {
+  # You have to send a "dir" variable
+  $errors[] = "DELETE requested, but no 'dir' provided";
+} elseif ($request_method == 'DELETE' && empty($errors)) {
+  $dir = stripslashes($dir);
+
+  # Actually run the directory removal command
+  $rm_command = "[ -e $dir ] && /bin/rm -rf '$doc_root/$dir'";
+  system($rm_command, $rm_failed);
+
+  # If there was a removal failure...
+  if ($rm_failed) { $errors[] = "Directory $dir/ could not be removed (may not exist)."; }
+
+  $result = "$dir removed.";
+}
+
+
 
 
 # OUTPUT HAPPENS BELOW
@@ -74,6 +107,6 @@ if (!empty ($errors)) {
   print "Errors: \n";
   print implode ("\n", $errors);
 } else {
-  # Otherwise print the address of the file you just uploaded.
-  print "$server_protocol://$server_name/$result_dir/$file_name";
+  # Otherwise print the result of whatever happened above
+  print $result;
 }
